30db8bc895b25b916e1b92e1709b71c83bc76bb8
Daniel <angelicodaniel93@gmail.com>
2022-10-21 17:24:58 +0200

    un po' di css

--- assets/main.js
+++ assets/main.js
@@ -3,17 +3,17 @@ let FizzBuzz = document.getElementById('fizzbuzz')
 for (let i = 1; i <= 100; i++) {
     let box;
     if ( i % 3 === 0 && i % 5 === 0){
-        box = `<div>FizzBuzz </div>`
+        box = `<div class="box-red">FizzBuzz </div>`
         console.log('FizzBuzz')
     } else if ( i  % 3 === 0){
-        box = `<div> Fizz </div>`
+        box = `<div class="box-blue"> Fizz </div>`
         console.log('Fizz')
     } else if ( i % 5 === 0) {
-        box = `<div> FizzBuzz </div>`
+        box = `<div class="box-green"> Buzz </div>`
         console.log('Buzz')
     } else {
         console.log(i)
-        box = `<div> ${i} </div>`
+        box = `<div class="box"> ${i} </div>`
     };
 
    FizzBuzz.innerHTML += box 
