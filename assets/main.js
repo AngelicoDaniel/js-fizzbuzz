a5ac6133bb77848d6acb4cc69c89a96146da9fdf
Daniel <angelicodaniel93@gmail.com>
2022-10-21 16:10:52 +0200

    funziona

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,22 @@
-let FizzBuzz = document.querySelector('box')
+let FizzBuzz = document.getElementById('fizzbuzz')
 
 for (let i = 1; i <= 100; i++) {
     let box;
     if ( i % 3 === 0 && i % 5 === 0){
-        box = `<div class= "box-red" FizzBuzz </div>`
+        box = `<div>FizzBuzz </div>`
         console.log('FizzBuzz')
     } else if ( i  % 3 === 0){
-        box = `<div class= "box-blue" Fizz </div>`
+        box = `<div> Fizz </div>`
         console.log('Fizz')
     } else if ( i % 5 === 0) {
-        box = `<div class= "box-red" FizzBuzz </div>`
+        box = `<div> FizzBuzz </div>`
         console.log('Buzz')
     } else {
         console.log(i)
+        box = `<div> ${i} </div>`
     };
 
-    FizzBuzz.innerHTML=box;
+   FizzBuzz.innerHTML += box 
 }
 
 
