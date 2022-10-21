55b81176025ed1c7a2d695b8b5cf9e8847ae8118
Daniel <angelicodaniel93@gmail.com>
2022-10-21 15:58:16 +0200

    box

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,28 @@
-//ciclo
-for (let i = 0; i <= 100; i++) {
+let FizzBuzz = document.querySelector('box')
+
+for (let i = 1; i <= 100; i++) {
+    let box;
     if ( i % 3 === 0 && i % 5 === 0){
+        box = `<div class= "box-red" FizzBuzz </div>`
         console.log('FizzBuzz')
     } else if ( i  % 3 === 0){
+        box = `<div class= "box-blue" Fizz </div>`
         console.log('Fizz')
     } else if ( i % 5 === 0) {
+        box = `<div class= "box-red" FizzBuzz </div>`
         console.log('Buzz')
     } else {
         console.log(i)
-    }
+    };
+
+    FizzBuzz.innerHTML=box;
 }
+
+
+
+
+
+
+
+
 
