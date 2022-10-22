252c230e5f6605ff30ee21c1d221a2f905bdc5ce
Daniel <angelicodaniel93@gmail.com>
2022-10-22 11:50:37 +0200

    append funzionante; last commit

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,31 @@
 let FizzBuzz = document.getElementById('fizzbuzz');
+
+//append
 let number = document.createElement('div');
+FizzBuzz.append(number);
 
 
 
-//FizzBuzz.append(number);
 
 for (let i = 1; i <= 100; i++) {
 
     if ( i % 3 === 0 && i % 5 === 0){
-        box = `<div class="box-fizzbuzz">FizzBuzz</div>`
+        number.innerHtml += `<div class="box-fizzbuzz">FizzBuzz</div>`
         console.log('FizzBuzz')
 
     } else if ( i  % 3 === 0){
-        box = `<div class="box-fizz"> Fizz </div>`
+        number.innerHTML += `<div class="box-fizz">Fizz</div>`
         console.log('Fizz')
 
     } else if ( i % 5 === 0) {
-        box = `<div class="box-buzz"> Buzz </div>`
+        number.innerHTML += `<div class="box-buzz">Buzz</div>`
         console.log('Buzz')
 
     } else {
         console.log(i)
-        box = `<div class="box"> ${i} </div>`
+        number.innerHTML += `<div class="box">${i}</div>`
     };
-
-   FizzBuzz.innerHTML += box 
+    
 }
 
 
